e5f497854d6fae339ae52fca92411f72a1504cf5
Francis-PC\Francis <francis94c@gmail.com>
2018-01-06 08:45:17 +0100

    Organizing the Code and Testing Compilation.

---
+++ atom.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+CLASSPATH="C:\Repositories\esp-partition-gui\sub-modules\ESP32Partitions\pde.jar" atom
--- src/ESP32Partitions.java
+++ src/ESP32Partitions.java
@@ -1,44 +1,42 @@
 package com.espressif.partitions;
 
-
-import java.lang.Runtime;
-
 import java.io.InputStream;
+import java.lang.Runtime;
+import java.lang.StringBuilder;
 
 import processing.app.Editor;
 import processing.app.tools.Tool;
 
-
 public class ESP32Partitions implements Tool {
-    
+
 	Editor editor;
 
+  public void init(Editor editor) {
+    this.editor = editor;
+  }
 
-    public void init(Editor editor) {
-        this.editor = editor;
+  public String getMenuTitle() {
+    return "ESP32 Partition Manager";
+  }
+
+  public void run() {
+		Runtime rt = Runtime.getRuntime();
+		try {
+			StringBuilder command = new StringBuilder("reg query \"HKCU\\Software\\");
+			command.append("Microsoft\\Windows\\CurrentVersion\\Explorer\\");
+			command.append("Shell Folders\" /v personal");
+			Process p = rt.exec(command.toString());
+      p.waitFor();
+      InputStream in = p.getInputStream();
+      byte[] b = new byte[in.available()];
+      in.read(b);
+      in.close();
+      String docsPath = new String(b);
+      docsPath = docsPath.split("\\s\\s+")[4] + "\\";
+      Process pr = rt.exec("python " + docsPath + "Arduino\\tools\\ESP32Partitions\\tool\\esp-partition.py");
+    } catch (Exception e) {
+			e.printStackTrace();
     }
+  }
 
-
-    public String getMenuTitle() {
-        return "ESP32 Partition Manager";
-    }
-
-
-    public void run() {
-        Runtime rt = Runtime.getRuntime();
-        try {
-            Process p = rt.exec("reg query \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Shell Folders\" /v personal");
-            p.waitFor();
-            InputStream in = p.getInputStream();
-            byte[] b = new byte[in.available()];
-            in.read(b);
-            in.close();
-            String docsPath = new String(b);
-            docsPath = docsPath.split("\\s\\s+")[4] + "\\";
-            Process pr = rt.exec("python " + docsPath + "Arduino\\tools\\ESP32Partitions\\tool\\esp-partition.py");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-	
 }
